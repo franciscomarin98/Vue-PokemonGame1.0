bec67bb10d9df8e2e6566dafa75b6b125418689a
franciscomarin98 <francmarinc@gmail.com>
2021-09-10 21:50:29 -0500

    Terminando la funcionalidad para generar las 4 opciones aleatoriamente

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,16 +1,40 @@
+import pokemonApi from "../api/pokemonApi";
+
 const getPokemons = () => {
     const pokemonsArray = Array.from(Array(650));
     return pokemonsArray.map((_, index) => index + 1);
 
 }
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async () => {
     const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5);
-    getPokemonsNames(mixedPokemons.slice(0, 4));
+    return await getPokemonsNames(mixedPokemons.slice(0, 4));
 }
 
-const getPokemonsNames = ([p1, p2, p3, p4]) => {
-    console.log(p1, p2, p3, p4);
+const getPokemonsNames = async ([p1, p2, p3, p4]) => {
+    let arrPromises;
+    arrPromises = [
+        pokemonApi.get(`/${p1}`),
+        pokemonApi.get(`/${p2}`),
+        pokemonApi.get(`/${p3}`),
+        pokemonApi.get(`/${p4}`),
+    ];
+
+    const [r1, r2, r3, r4] = await Promise.all(arrPromises);
+    return [
+        {
+            name: r1.data.name, id: r1.data.id
+        },
+        {
+            name: r2.data.name, id: r2.data.id
+        },
+        {
+            name: r3.data.name, id: r3.data.id
+        },
+        {
+            name: r4.data.name, id: r4.data.id
+        },
+    ]
 }
 
 export default getPokemonOptions;
